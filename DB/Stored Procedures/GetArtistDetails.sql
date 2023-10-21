516a48d6bb392e77e9922875f21f70abc3ebe63c
Chibuikem Akpaka <lextutor.dev@gmail.com>
2023-10-21 20:09:19 +0100

    Artist details page implementation.

---
+++ DB/Stored Procedures/GetArtistDetails.sql
@@ -0,0 +1,44 @@
+﻿CREATE PROCEDURE [dbo].[GetArtistDetails]
+    @artistID INT
+AS
+BEGIN
+    -- Subquery for Artist Details
+    SELECT 
+        artist.title AS ArtistName,
+        artist.imageURL AS ArtistImage,
+        artist.biography AS ArtistBiography,
+        artist.heroURL AS ArtistBannerImage
+    FROM 
+        dbo.[Artist] artist
+    WHERE 
+        artist.artistID = @artistID;
+
+    -- Subquery for Song Details
+    SELECT 
+        song.title AS SongTitle,
+        song.bpm AS BeatsPerMinute,
+        (CONVERT(INT, song.multitracks) + CONVERT(INT, song.customMix) +
+        CONVERT(INT, song.chart) + CONVERT(INT, song.rehearsalMix) +
+        CONVERT(INT, song.patches) + CONVERT(INT, song.proPresenter)) AS FeatureCount,
+        album.title AS AlbumName,
+        album.imageURL AS AlbumImage
+    FROM 
+        dbo.[Song] song
+    LEFT JOIN 
+        dbo.[Album] album ON song.albumID = album.albumID
+    WHERE 
+        song.artistID = @artistID;
+
+    -- Subquery for Album Details
+    SELECT 
+        album.title AS AlbumTitle,
+        album.imageURL AS AlbumImage,
+        album.year AS AlbumYear,
+        artist.title AS AlbumArtist
+    FROM 
+        dbo.[Album] album 
+    JOIN 
+        dbo.[Artist] artist ON album.artistID = artist.artistID
+    WHERE 
+        album.artistID = @artistID;
+END
---
+++ Web/multitracks.com/multitracks.com/artistDetails.aspx.cs
@@ -0,0 +1,64 @@
+﻿using DataAccess;
+using System;
+using static System.Net.WebRequestMethods;
+
+public partial class artistDetails : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        try
+        {
+            var sql = new SQL();
+
+            if (!int.TryParse(Request.QueryString["id"], out int artistId) || artistId == 0)
+            {
+                artistId = 5; //A default as Hillsong because I love their songs.
+            }
+
+            sql.Parameters.Add("@artistID", artistId);
+            var artistDetailsDataSet = sql.ExecuteStoredProcedureDS("GetArtistDetails", true);
+
+            if (artistDetailsDataSet != null || artistDetailsDataSet.Tables.Count > 0)
+            {
+                var artistInformations = artistDetailsDataSet?.Tables[0].ToDynamic();
+
+                if (artistInformations != null && artistInformations.Count > 0)
+                {
+                    var artistInformation = artistInformations[0];
+
+                    artistBannerImage.ImageUrl = artistInformation.ArtistBannerImage;
+                    artistBannerImage.Attributes["srcset"] = $"{artistInformation.ArtistBannerImage}, {artistInformation.ArtistBannerImage} 2x";
+                    artistBannerImage.AlternateText = artistInformation.ArtistName;
+
+                    artistImage.ImageUrl = artistInformation.ArtistImage;
+                    artistImage.Attributes["srcset"] = $"{artistInformation.ArtistImage}, {artistInformation.ArtistImage} 2x";
+                    artistImage.AlternateText = artistInformation.ArtistImage;
+
+                    artistLabel.Text = artistInformation.ArtistName;
+
+                    emptyContent.Visible = false;
+                    artistContent.Visible = true;
+                }
+                else
+                {
+                    emptyContent.Visible = true;
+                    artistContent.Visible = false;
+                }
+
+                BiographyFormView.DataSource = artistDetailsDataSet.Tables[0];
+                BiographyFormView.DataBind();
+
+                Songs.DataSource = artistDetailsDataSet.Tables[1];
+                Songs.DataBind();
+
+                Albums.DataSource = artistDetailsDataSet.Tables[2];
+                Albums.DataBind();
+            }
+        }
+        catch (Exception)
+        {
+
+            throw;
+        }
+    }
+}
